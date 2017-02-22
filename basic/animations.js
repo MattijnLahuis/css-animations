f8f89e4675a2fd318035737409b6619cf89ba11c
Mattijn <mattijnlahuis@gmail.com>
2017-02-22 10:39:46 +0100

    curved ball

--- basic/animations.js
+++ basic/animations.js
@@ -1,17 +1,22 @@
 function startAnimation() {
-  /* js stuff goes here */
+  // let container = document.querySelector('.animationContainer');
+  // while (container.hasChildNodes()) {
+  //   container.removeChild(container.lastChild);
+  // }
 
+  let container = document.createElement('div');
 
-  let container = document.querySelector('.animationContainer');
-
-  while (container.hasChildNodes()) {
-    container.removeChild(container.lastChild);
-  }
+  container.className = 'animationContainer';
 
   let animation = document.createElement('div');
 
   animation.className = "animation";
   animation.style['animation-duration'] = '1s';
 
-  document.querySelector('.animationContainer').appendChild(animation);
+  container.appendChild(animation);
+  document.querySelector('body').appendChild(container);
+
+  container.addEventListener('animationend', function(event) {
+    event.target.parentNode.removeChild(event.target);
+  })
 }
